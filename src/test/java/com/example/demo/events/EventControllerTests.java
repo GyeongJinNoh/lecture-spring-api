7aff5b491174eab7f9afe3542f92cc0d2d67856e
GyeongJinNoh <yesgjin@gmail.com>
2023-07-19 20:12:33 +0900

    Add test description annotation

--- src/test/java/com/example/demo/events/EventControllerTests.java
+++ src/test/java/com/example/demo/events/EventControllerTests.java
@@ -1,5 +1,6 @@
 package com.example.demo.events;
 
+import com.example.demo.common.TestDescription;
 import com.example.demo.events.Event;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.hamcrest.Matcher;
@@ -37,6 +38,7 @@ public class EventControllerTests {
     ObjectMapper objectMapper;
 
     @Test
+    @TestDescription("정상적으로 이벤트를 생성하는 테스트")
     public void createEvent() throws Exception {
         EventDto event = EventDto.builder()
                 .name("Spring")
@@ -70,6 +72,7 @@ public class EventControllerTests {
     }
 
     @Test
+    @TestDescription("입력 받을 수 없는 값을 사용한 경우에 에러가 발생하는 테스트")
     public void createEvent_Bad_Request() throws Exception {
         Event event = Event.builder()
                 .id(100)
@@ -96,6 +99,7 @@ public class EventControllerTests {
                 .andExpect(status().isBadRequest());
     }
 
+    @TestDescription("입력 값이 비어있는 경우에 에러가 발생하는 테스트")
     @Test
     public void createEvent_Bad_Request_Empty_Input() throws Exception {
         EventDto eventDto = EventDto.builder().build();
@@ -106,6 +110,7 @@ public class EventControllerTests {
                     .andExpect(status().isBadRequest());
     }
 
+    @TestDescription("입력 값이 잘못된 경우에 에러가 발생하는 테스트")
     @Test
     public void createEvent_Bad_Request_Wrong_Input() throws Exception {
         EventDto eventDto = EventDto.builder()
