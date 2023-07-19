40be81c432272e836ad1330941ae7aa6f9ed8efd
GyeongJinNoh <yesgjin@gmail.com>
2023-07-19 19:59:10 +0900

    validate wrong data

--- src/test/java/com/example/demo/events/EventControllerTests.java
+++ src/test/java/com/example/demo/events/EventControllerTests.java
@@ -96,5 +96,35 @@ public class EventControllerTests {
                 .andExpect(status().isBadRequest());
     }
 
+    @Test
+    public void createEvent_Bad_Request_Empty_Input() throws Exception {
+        EventDto eventDto = EventDto.builder().build();
+
+        this.mockMvc.perform(post("/api/events")
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .content(this.objectMapper.writeValueAsString(eventDto)))
+                    .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void createEvent_Bad_Request_Wrong_Input() throws Exception {
+        EventDto eventDto = EventDto.builder()
+                .name("Spring")
+                .description("REST API Development with Spring")
+                .beginEnrollmentDateTime(LocalDateTime.of(2023, 07, 18, 19, 54))
+                .closeEnrollmentDateTime(LocalDateTime.of(2023, 07, 18, 19, 54))
+                .beginEventDateTime(LocalDateTime.of(2023, 07, 19, 19, 54))
+                .endEventDateTime(LocalDateTime.of(2023, 07, 18, 19, 54))
+                .basePrice(10000)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("강남역 D2 스타텁 팩토리")
+                .build();
+
+        this.mockMvc.perform(post("/api/events")
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .content(this.objectMapper.writeValueAsString(eventDto)))
+                .andExpect(status().isBadRequest());
+    }
 
 }
